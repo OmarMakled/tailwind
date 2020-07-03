4bfd46fba05a4555bf92c4b2f2a5a91b42cff9b5
Omar <omar.makled@gmail.com>
2020-07-03 11:36:06 +0200

    wip

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <chit-chat></chit-chat>
+    <erin-lindford class="mt-4"></erin-lindford>
+  </div>
 </template>
 
 <script>
-
+import ChitChat from "@/components/ChitChat";
+import ErinLindford from "@/components/ErinLindford";
 export default {
   name: "home",
-
+  components: { ChitChat, ErinLindford }
 };
 </script>
