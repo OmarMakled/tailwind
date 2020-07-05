2c41d6871b4a056975a05c0fd62566e60bb7e7d3
Omar <omar.makled@gmail.com>
2020-07-05 15:50:27 +0200

    wip

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>Never stop learning</div>
+  <main>
+    <div class="p-10">
+      <div class>
+        <img src="@/assets/img/undraw_personal_email_t7nw.svg" alt />
+      </div>
+      <div class="mt-10 text-center">
+        <h3 class="font-semibold text-gray-900">lorem4</h3>
+        <p
+          class="text-gray-800 mt-6"
+        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, facilis?</p>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
