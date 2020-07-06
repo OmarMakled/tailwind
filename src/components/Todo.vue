0754c537386bb1f9d664645694fcd643be818129
Omar <omar.makled@gmail.com>
2020-07-06 06:26:03 +0200

    nice work

---
+++ src/components/Todo.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="flex items-center min-h-screen p-10">
+    <div class="md:text-left text-center">
+      <h1 class="font-bold text-5xl">A task manager you can trust for life</h1>
+      <p
+        class="mt-3 text-xl"
+      >In the 13 years and 158 days that we’ve been building Todoist, we’ve never considered selling out or becoming acquired.</p>
+      <p
+        class="mt-3 text-xl"
+      >Our team is committed to staying independent and earning your trust for as long as you need our apps.</p>
+      <div class="flex items-center justify-center md:justify-start mt-3">
+        <svg
+          fill="none"
+          height="24"
+          viewBox="0 0 24 24"
+          width="24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M18.707 11.267l-4.94-5.115a.487.487 0 0 0-.706 0 .53.53 0 0 0 0 .732s4.457 4.602 4.455 4.602H5.5a.51.51 0 0 0-.5.518c0 .286.224.518.5.518h12.016l-4.455 4.594a.531.531 0 0 0 0 .733.488.488 0 0 0 .707 0l4.94-5.116c.39-.405.39-1.061 0-1.466z"
+            fill="#316fea"
+          />
+        </svg>
+        <a href class="font-light text-blue-500">Learn about our long-term mission</a>
+      </div>
+    </div>
+    <div class="hidden md:block">
+      <img
+        src="https://todoist.com/_next/static/images/mission@2x_14c0f4a5fedd3c66ca5d91982d823c97.webp"
+        alt
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,16 @@
 <template>
-  <main class="flex items-center min-h-screen p-10">
-    <div class="md:text-left text-center">
-      <h1 class="font-bold text-5xl">A task manager you can trust for life</h1>
+  <main class="flex flex-col justify-center min-h-screen p-10">
+    <div class="text-center">
+      <h1 class="font-semibold text-4xl">Keep your family safe online</h1>
       <p
-        class="mt-3 text-xl"
-      >In the 13 years and 158 days that we’ve been building Todoist, we’ve never considered selling out or becoming acquired.</p>
-      <p
-        class="mt-3 text-xl"
-      >Our team is committed to staying independent and earning your trust for as long as you need our apps.</p>
-      <div class="flex items-center justify-center md:justify-start mt-3">
-        <svg
-          fill="none"
-          height="24"
-          viewBox="0 0 24 24"
-          width="24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M18.707 11.267l-4.94-5.115a.487.487 0 0 0-.706 0 .53.53 0 0 0 0 .732s4.457 4.602 4.455 4.602H5.5a.51.51 0 0 0-.5.518c0 .286.224.518.5.518h12.016l-4.455 4.594a.531.531 0 0 0 0 .733.488.488 0 0 0 .707 0l4.94-5.116c.39-.405.39-1.061 0-1.466z"
-            fill="#316fea"
-          />
-        </svg>
-        <a href class="font-light text-blue-500">Learn about our long-term mission</a>
-      </div>
+        class="mt-6 text-lg"
+      >The easiest and safest way to share logins, passwords, credit cards and more, with the people that matter most. Go ahead, forget your passwords – 1Password remembers them all for you.</p>
+      <button class="bg-blue-500 mt-4 px-8 py-3 rounded-full text-white">Try free for 30 Days</button>
     </div>
-    <div class="hidden md:block">
+    <div class="mt-8">
       <img
-        src="https://todoist.com/_next/static/images/mission@2x_14c0f4a5fedd3c66ca5d91982d823c97.webp"
+        class="mx-auto w-auto"
+        src="https://1password.com/img/redesign/homepage/family-illustration.f70d5d9ecd3c8cc5371e5d8b841afa36.svg"
         alt
       />
     </div>
