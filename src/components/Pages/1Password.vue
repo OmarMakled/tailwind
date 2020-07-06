a74f6380aa66b9dc3362762e3fe42c405d21b797
Omar <omar.makled@gmail.com>
2020-07-06 06:44:12 +0200

    great job

---
+++ src/components/Pages/1Password.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col justify-center md:flex-row md:items-center min-h-screen p-10">
+    <div class="md:text-left md:w-1/3 text-center">
+      <h1 class="font-semibold text-4xl">Keep your family safe online</h1>
+      <p
+        class="mt-6 text-lg"
+      >The easiest and safest way to share logins, passwords, credit cards and more, with the people that matter most. Go ahead, forget your passwords – 1Password remembers them all for you.</p>
+      <button class="bg-blue-500 mt-4 px-8 py-3 rounded-full text-white">Try free for 30 Days</button>
+    </div>
+    <div class="md:flex-grow md:mt-0 mt-8 mx-auto">
+      <img
+        class="w-full"
+        src="https://1password.com/img/redesign/homepage/family-illustration.f70d5d9ecd3c8cc5371e5d8b841afa36.svg"
+        alt
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,5 @@
 <template>
-  <main class="flex flex-col justify-center min-h-screen p-10">
-    <div class="text-center">
-      <h1 class="font-semibold text-4xl">Keep your family safe online</h1>
-      <p
-        class="mt-6 text-lg"
-      >The easiest and safest way to share logins, passwords, credit cards and more, with the people that matter most. Go ahead, forget your passwords – 1Password remembers them all for you.</p>
-      <button class="bg-blue-500 mt-4 px-8 py-3 rounded-full text-white">Try free for 30 Days</button>
-    </div>
-    <div class="mt-8">
-      <img
-        class="mx-auto w-auto"
-        src="https://1password.com/img/redesign/homepage/family-illustration.f70d5d9ecd3c8cc5371e5d8b841afa36.svg"
-        alt
-      />
-    </div>
-  </main>
+  <main></main>
 </template>
 
 <script>
