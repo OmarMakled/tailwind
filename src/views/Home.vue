a395f53a0a82228ee954c18f9eab16b51fc82a24
Omar <omar.makled@gmail.com>
2020-07-06 05:44:43 +0200

    great job

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,34 @@
 <template>
-  <main>
-    <div class="p-10">
-      <div class>
-        <img src="@/assets/img/undraw_personal_email_t7nw.svg" alt />
+  <main class="flex items-center min-h-screen p-10">
+    <div>
+      <h1 class="font-bold text-5xl">A task manager you can trust for life</h1>
+      <p
+        class="mt-3 text-xl"
+      >In the 13 years and 158 days that we’ve been building Todoist, we’ve never considered selling out or becoming acquired.</p>
+      <p
+        class="mt-3 text-xl"
+      >Our team is committed to staying independent and earning your trust for as long as you need our apps.</p>
+      <div class="flex items-center mt-3 text-xl">
+        <svg
+          fill="none"
+          height="24"
+          viewBox="0 0 24 24"
+          width="24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M18.707 11.267l-4.94-5.115a.487.487 0 0 0-.706 0 .53.53 0 0 0 0 .732s4.457 4.602 4.455 4.602H5.5a.51.51 0 0 0-.5.518c0 .286.224.518.5.518h12.016l-4.455 4.594a.531.531 0 0 0 0 .733.488.488 0 0 0 .707 0l4.94-5.116c.39-.405.39-1.061 0-1.466z"
+            fill="#316fea"
+          />
+        </svg>
+        <a href class="font-light text-blue-500">Learn about our long-term mission</a>
       </div>
-      <div class="mt-10 text-center">
-        <h3 class="font-semibold text-gray-900">lorem4</h3>
-        <p
-          class="text-gray-800 mt-6"
-        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, facilis?</p>
-      </div>
+    </div>
+    <div>
+      <img
+        src="https://todoist.com/_next/static/images/mission@2x_14c0f4a5fedd3c66ca5d91982d823c97.webp"
+        alt
+      />
     </div>
   </main>
 </template>
