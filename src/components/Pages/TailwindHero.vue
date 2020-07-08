2e820940b872cd02ebf67d3fdd9c5d37d57be939
Omar <omar.makled@gmail.com>
2020-07-08 11:24:30 +0200

    lg

--- src/components/Pages/TailwindHero.vue
+++ src/components/Pages/TailwindHero.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="p-10">
     <div class="flex justify-between">
-      <img src="https://tailwindui.com/img/logos/workflow-mark-on-white.svg" class="w-8" alt />
-      <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+      <img src="https://tailwindui.com/img/logos/workflow-mark-on-white.svg" alt class="w-8" />
+      <svg stroke="currentColor" fill="none" viewBox="0 0 24 24" class="block h-6 lg:hidden w-6">
         <path
           stroke-linecap="round"
           stroke-linejoin="round"
@@ -10,31 +10,40 @@
           d="M4 6h16M4 12h16M4 18h16"
         />
       </svg>
-    </div>
-
-    <div class="mt-8">
-      <h1 class="font-black leading-9 md:leading-normal md:text-5xl md:text-center text-3xl">
-        Data to enrich your
-        <span class="block text-indigo-500">online business</span>
-      </h1>
-      <p
-        class="font-light md:text-center mt-5 text-xl"
-      >Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
-      <div class="md:flex md:justify-center mt-5">
-        <button
-          class="bg-indigo-500 block md:mr-4 md:w-auto px-10 py-3 rounded-md text-white w-full"
-        >Get Started</button>
-        <button
-          class="bg-indigo-100 block md:mt-0 md:w-auto mt-5 px-10 py-3 rounded-md text-indigo-500 w-full"
-        >Live Demo</button>
+      <div class="hidden lg:block">
+        <a href class="px-6 py-6 text-gray-800">Product</a>
+        <a href class="px-6 py-6 text-gray-800">Features</a>
+        <a href class="px-6 py-6 text-gray-800">Marketplace</a>
+        <a href class="px-6 py-6 text-gray-800">Company</a>
+        <a href class="px-4 py-4 text-indigo-600">Log in</a>
       </div>
     </div>
-
-    <div class="md:mt-12 mt-8">
-      <img
-        src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-        alt
-      />
+    <div class="lg:flex">
+      <div class="mt-8">
+        <h1
+          class="font-black leading-9 lg:text-left md:leading-normal md:text-5xl md:text-center text-3xl"
+        >
+          Data to enrich your
+          <span class="block text-indigo-500">online business</span>
+        </h1>
+        <p
+          class="font-light lg:text-left md:text-center mt-5 text-xl xl:-m-1"
+        >Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+        <div class="lg:justify-start md:flex md:justify-center mt-5">
+          <button
+            class="bg-indigo-500 block md:mr-4 md:w-auto px-10 py-3 rounded-md text-white w-full"
+          >Get Started</button>
+          <button
+            class="bg-indigo-100 block md:mt-0 md:w-auto mt-5 px-10 py-3 rounded-md text-indigo-500 w-full"
+          >Live Demo</button>
+        </div>
+      </div>
+      <div class="lg:w-11/12 md:mt-12 mt-8">
+        <img
+          src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2850&amp;q=80"
+          alt
+        />
+      </div>
     </div>
   </div>
 </template>
