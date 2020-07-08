7228d91b14e32841810c38d1b546d6e1822897b5
Omar <omar.makled@gmail.com>
2020-07-08 10:42:03 +0200

    tailwind sm

---
+++ src/components/Pages/TailwindHero.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="p-10">
+    <div class="flex justify-between">
+      <img src="https://tailwindui.com/img/logos/workflow-mark-on-white.svg" class="w-8" alt />
+      <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 12h16M4 18h16"
+        />
+      </svg>
+    </div>
+
+    <div class="mt-8">
+      <h1 class="font-black leading-9 text-3xl">
+        Data to enrich your
+        <span class="block text-indigo-500">online business</span>
+      </h1>
+      <p
+        class="font-light mt-5 text-xl"
+      >Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+      <button class="bg-indigo-500 block mt-5 px-10 py-3 rounded-md text-white w-full">Get Started</button>
+      <button
+        class="bg-indigo-100 block mt-5 px-10 py-3 rounded-md text-indigo-500 w-full"
+      >Live Demo</button>
+    </div>
+
+    <div class="mt-8">
+      <img
+        src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+        alt
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,15 @@
 <template>
-  <main></main>
+  <main>
+    <tailwind-hero></tailwind-hero>
+  </main>
 </template>
 
 <script>
+import TailwindHero from "@/components/Pages/TailwindHero";
 export default {
   name: "home",
-  components: {}
+  components: {
+    TailwindHero
+  }
 };
 </script>
